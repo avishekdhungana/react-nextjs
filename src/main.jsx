b24d1c1cbf0c07be18d69428ba4d14325ce40175
Avishek Dhungana <avishekdhungana38@gmail.com>
2025-08-10 18:59:44 +0545

    style added

--- src/main.jsx
+++ src/main.jsx
@@ -26,10 +26,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import Newapp from './Newapp.jsx';
+// import HabitForm from './Habittracker/HabitForm.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Newapp />
+    <Newapp/>
   </StrictMode>
 );
 >>>>>>> a969eeb (new task)
