ce920334da93859f062ad99c4e13ceedb1ede45a
Avishek Dhungana <avishekdhungana38@gmail.com>
2025-08-11 22:14:03 +0545

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,11 @@
-<<<<<<< HEAD
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-<<<<<<< HEAD
-import Filter from './Filter.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-<<<<<<< HEAD
-    <Filter />
-=======
+import Todo from './Todo.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
->>>>>>> 40afa96 (1st commit)
-=======
-    <App />
->>>>>>> 1645fa9 ('fixes')
-  </StrictMode>,
-)
-=======
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import Newapp from './Newapp.jsx';
-// import HabitForm from './Habittracker/HabitForm.jsx';
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Newapp/>
   </StrictMode>
-);
->>>>>>> a969eeb (new task)
+)
