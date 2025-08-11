a9d8abc3f3854cb12d982117d38a5196093f1707
Avishek Dhungana <avishekdhungana38@gmail.com>
2025-08-11 22:32:09 +0545

    changed

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Todo from './Todo.jsx'
+import TaskManager from './TaskManager.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
